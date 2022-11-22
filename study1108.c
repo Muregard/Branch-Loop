afdf5b4702662df08fd16a7d817253e424d9d21f
Muregard <117261085+Muregard@users.noreply.github.com>
2022-11-22 14:57:53 +0800

    Add files via upload

---
+++ study1108.c
@@ -0,0 +1,223 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+//int main()
+//{
+//	int a;
+//	printf("请输入整数\n");
+//	scanf("%d",&a);
+//	if (a % 2 == 0)
+//		printf("偶数\n");
+//	else
+//		printf("奇数\n");
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	while (i <=100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("今天是星期一\n");
+//		break;
+//	case 2:
+//		printf("今天是星期二\n");
+//		break;
+//	case 3:
+//		printf("今天是星期三\n");
+//		break;
+//	case 4:
+//		printf("今天是星期四\n");
+//		break;
+//	case 5:
+//		printf("今天是星期五\n");
+//		break;
+//	case 6:
+//		printf("今天是星期六\n");
+//		break;
+//	case 7:
+//		printf("今天是星期天\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("工作日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		printf("%d ",i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+
+//int main()
+//{
+//	char password[20] = { 0 };
+//	int ret = 0;
+//	int ch = 0;
+//	printf("请输入密码\n");
+//	scanf("%s", password);
+//	while (ch = getchar() != '\n')//持续读取缓冲区字符，直到出现'\n'
+//	{
+//		;
+//	}
+//	printf("请确认(Y/N)\n");
+//	ret = getchar();
+//	if (ret == 'Y')
+//		printf("确认成功\n");
+//	else
+//		printf("放弃确认\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i;
+//	for (i = 1;i <=10;i++)
+//		printf("%d ", i);
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	do
+//	{
+//		i++;
+//		printf("%d ", i);
+//	} 
+//	while (i<10);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int n=0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1;i <= n;i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int sum = 0;
+//	int ret = 1;
+//	for (i = 1,sum=0;i <= 10;i++)
+//	{
+//		for (n = 1,ret=1;n <= i;n++)
+//		{
+//			ret *= n;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n",sum);
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	char password[20] = {0};
+	printf("请输入密码\n");
+	for (i = 0;i < 3;i++)
+	{
+		scanf("%s", password);
+		if(strcmp(password,"aahph")==0)
+		{
+			printf("成功登录\n");
+			break;
+		}
+		else
+		{
+			printf("密码错误\n");
+		}
+	}
+	if (i == 3)
+		printf("三次密码均错误，退出程序\n");
+	return 0;
+}
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0;i < sz;i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了,下标是：%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//	return 0;
+//}
